b6c8755e3f2184b4ed04c0eb1dd17329c939d9c8
lavish <lavishjain36@gmil.com>
2022-10-18 21:36:58 +0530

    Added addto cart

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-
+import Cards from "./components/Cards";
+import data from "./data/data.json"
+import { useState } from "react";
 
 const App=()=>{
+  const [itemCount,setItemCount]=useState(0)
+
+  //function to check type of button for calling related functions
+  const manageItems=(event)=>{
+    if(event.target.innerText==="Add to cart")
+    {addItem(event)}
+    else if(event.target.innerText==="Remove from cart"){
+      removeItem(event);
+    }
+  }
+
+  //Add item function to increase the item in the cart
+  const addItem=(event)=>{
+    setItemCount(()=>itemCount+1);
+    event.target.innerText="Remove from cart";
+    event.target.classList.value="btn btn-danger"
+
+  }
+
+  //Remove item function to decrease the item in the cart
+  const removeItem=(event)=>{
+    setItemCount(()=>itemCount-1);
+    event.target.innerText="Add to cart";
+    event.target.classList.value="btn btn-primary";
+
+  };
   return(
     <>
-      <Navbar/>
+      <Navbar itemCount={itemCount}/>
       <Header/>
-      <Cards/>
+      <Cards data={data} manageItems={manageItems}/>
       <Footer/>
     </>
 
